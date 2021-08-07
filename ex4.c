9ea489373a55f41f44c1af1726e3400ea91fb8da
João Pedro de Almeida Campos <76006725+camp944@users.noreply.github.com>
2021-08-07 11:23:57 -0300

    Add files via upload

---
+++ ex4.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lista.h"
+#include "fila.h"
+#include "pilha.h"
+#include "base.h"
+#include "ex4.h"
+#define MAX 100
+
+void resolve4()
+{
+	
+	
+	
+	
+}
---
+++ ex4.h
@@ -0,0 +1,6 @@
+#ifndef EX4_H
+#define EX4_H
+
+void resolve4();
+
+#endif
